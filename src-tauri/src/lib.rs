2c1cedf7cb11c877b9ca00524d447854f9c63e99
Kwengang Tankeu <dannickstark@gmail.com>
2024-03-06 00:42:24 +0100

    basic embedded surrealdb

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,12 +1,65 @@
+pub mod structures;
+
+use core::result::Result::Ok;
+use surrealdb::engine::local::Mem;
+use surrealdb::Surreal; 
+
+use structures::{Person, Name, Record, Responsibility};
+
+
 #[tauri::command]
 fn greet(name: &str) -> String {
-   format!("Hello, {}!", name)
+    format!("Hello, {}!", name)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
-pub fn run() {
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![greet])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+pub async fn run() {
+    let _ = set_up_surreal_db().await;
+
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+async fn set_up_surreal_db() -> anyhow::Result<()> {
+    // Create database connection
+    let db = Surreal::new::<Mem>(()).await?;
+
+    // Select a specific namespace / database
+    db.use_ns("test").use_db("test").await?;
+
+    // Create a new person with a random id
+    let created: Vec<Record> = db
+        .create("person")
+        .content(Person {
+            title: "Founder & CEO",
+            name: Name {
+                first: "Dann",
+                last: "Stark",
+            },
+            marketing: false,
+        })
+        .await?;
+    dbg!(created);
+
+    // Update a person record with a specific id
+    let updated: Option<Record> = db
+        .update(("person", "jaime"))
+        .merge(Responsibility { marketing: true })
+        .await?;
+    dbg!(updated);
+
+    // Select all people records
+    let people: Vec<Record> = db.select("person").await?;
+    dbg!(people);
+
+    // Perform a custom advanced query
+    let groups = db
+        .query("SELECT marketing, count() FROM type::table($table) GROUP BY marketing")
+        .bind(("table", "person"))
+        .await?;
+    dbg!(groups);
+
+    Ok(())
 }
---
+++ src-tauri/src/structures.rs
@@ -0,0 +1,26 @@
+use serde::{Deserialize, Serialize};
+use surrealdb::sql::Thing;
+
+#[derive(Debug, Serialize)]
+pub struct Name<'a> {
+    pub first: &'a str,
+    pub last: &'a str,
+}
+
+#[derive(Debug, Serialize)]
+pub struct Person<'a> {
+    pub title: &'a str,
+    pub name: Name<'a>,
+    pub marketing: bool,
+}
+
+#[derive(Debug, Serialize)]
+pub struct Responsibility {
+    pub marketing: bool,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Record {
+    #[allow(dead_code)]
+    pub id: Thing,
+}
